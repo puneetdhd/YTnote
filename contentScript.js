9573b8a896a8c08cf2ec70f57405bd027bbe9e5b
Puneet <puneetkumardhal@gmail.com>
2025-02-23 22:19:07 +0530

    updates

--- contentScript.js
+++ contentScript.js
@@ -8,5 +8,24 @@
             currentVideo=videoId;
             newVideoLoaded();
         }
-    })
+    });
+
+    const newVideoLoaded=()=>{
+        const bookmarkBtnExists=document.getElementsByClassName("bookmark-btn")[0];
+        if(!bookmarkBtnExists){
+            const bookmarkBtn=document.createElement("img");
+
+            bookmarkBtn.src= chrome.runtime.getURL("assets/bookmark.png");
+            bookmarkBtn.className="ytp-button"+"bookmark-btn";
+            bookmarkBtn.title="Click to bookmark current timestamp";
+        
+            youtubeLeftControls=document.getElementsByClassName('ytp-left-controls')[0];
+            youtubePlayer=document.getElementsByClassName("video-stream")[0];
+
+            youtubeLeftControls.appendChild(bookmarkBtn); 
+            bookmarkBtn.addEventListener("click",addNewBookmarkEventHAndler)
+        }
+
+    }
+    newVideoLoaded();
 })
