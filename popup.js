470782fb2b597317b3cd2ad0d168c8cebfbb49b7
Puneet <puneetkumardhal@gmail.com>
2025-03-03 09:11:39 +0530

    6th commit

--- popup.js
+++ popup.js
@@ -1,9 +1,34 @@
 import { getActiveTabURL } from "./utils";
 
 //adding a new bookmark row to the popup
-const addNeweBookmark=()=>{};
+const addNewBookmark=(bookmarksElement,bookmark)=>{
+    const bookmarkTitleElement=document.createElement("div");
+    const newBookmarkElement=document.createElement("div");
 
-const viewBookmarks=()=>{};
+    bookmarkTitleElement.textContent=bookmark.desc;
+    bookmarkTitleElement.className="bookmark-title";
+
+    newBookmarkElement.id="bookmark-"+bookmark.time;
+    newBookmarkElement.className="bookmark";
+    newBookmarkElement.setAttribute("timestamp",bookmark.time);
+    
+    newBookmarkElement.appendChild(bookmarkTitleElement);
+    bookmarksElement.appendChild(newBookmarkElement);
+
+};
+
+const viewBookmarks=(currentBookmarks=[])=>{
+    const bookmarksElement=document.getElementsByClassName("bookmarks");
+    bookmarksElement.innerHTML=""; 
+    if(currentBookmarks.length>0){
+        for(let i=0;i<currentBookmarks.length;i++){
+            const bookmark=currentBookmarks[i];
+            addNeweBookmark(bookmarksElement,bookmark);
+        }
+    }else{
+        bookmarksElement.innerHTML='<div class="row">No bookmarks available</div>';
+    }
+};
 
 const onPlay=e=>{};
 
@@ -15,6 +40,7 @@ document.addEventListener("DOMContentLoaded",async()=>{
     const activeTab=await getActiveTabURL();
     const queryParameteres=activeTab.url.split("?")[1];
     const urlParameteres=new URLSearchParams(queryParameteres);
+    //converts it to key value pairs which can be further used
 
     const currentVideo=urlParameteres.get("v");
 
